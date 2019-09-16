1361459842696722ca3c3f2ebe42b7514bf171fa
chienchih <chien@xiaoxianroude-MacBook-Pro.local>
2019-09-16 16:06:34 -0700

    completed props introduction

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 // TODO
 /* Inside app.js, create a GroceryList component that contains an unordered list of 2 grocery items. Render this component to the div tag in index.html with an id of app*/
-var GroceryList = () => (
-      <ul>
-        <Milk />
-        <Bread />
-      </ul>
+var GroceryList = (props) => (
+  <ul>
+    <li>{props.grocery[0]}</li>
+    <li>{props.grocery[1]}</li>
+    <li>{props.grocery[2]}</li>
+  </ul>
 );
 
-var Milk = () => (
+var App = () => (
   <div>
-    <div>Milk</div>
-    <div>Cheese</div>
+    <h2>My GroceryList List</h2>
+    <GroceryList grocery={['Bread', 'Honey', 'Milk']}/>
   </div>
 );
 
-var Bread = () => (
-  <div>Bread</div>
-);
-
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
