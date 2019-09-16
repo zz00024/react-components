b8ff65e5514a815c168499e3daabbdf847517114
chienchih <chien@xiaoxianroude-MacBook-Pro.local>
2019-09-16 15:34:48 -0700

    complete first two part of the reac component

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // TODO
+/* Inside app.js, create a GroceryList component that contains an unordered list of 2 grocery items. Render this component to the div tag in index.html with an id of app*/
+var GroceryList = () => (
+      <ul>
+        <Milk />
+        <Bread />
+      </ul>
+);
+
+var Milk = () => (
+  <div>
+    <div>Milk</div>
+    <div>Cheese</div>
+  </div>
+);
+
+var Bread = () => (
+  <div>Bread</div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
