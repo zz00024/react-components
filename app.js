5a0f26a7e82e0f57f4919551fdc610d19de1a32f
chienchih <chien@xiaoxianroude-MacBook-Pro.local>
2019-09-16 16:41:09 -0700

    clickevent completed

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 // TODO
 /* Inside app.js, create a GroceryList component that contains an unordered list of 2 grocery items. Render this component to the div tag in index.html with an id of app*/
-var GroceryList = (props) => (
-  <ul>
-    <li>{props.grocery[0]}</li>
-    <li>{props.grocery[1]}</li>
-    <li>{props.grocery[2]}</li>
-  </ul>
-);
+var GroceryList = (props) => {
 
-var App = () => (
+
+
+  var onListItemClick = (event) => {
+    console.log('I got clicked');
+  };
+
+  return (
+    <ul>
+      <li onClick={onListItemClick}>{props.grocery[0]}</li>
+      <li>{props.grocery[1]}</li>
+      <li>{props.grocery[2]}</li>
+    </ul>
+  );
+}
+
+var GroceryListItem = () => (
   <div>
     <h2>My GroceryList List</h2>
     <GroceryList grocery={['Bread', 'Honey', 'Milk']}/>
   </div>
 );
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<GroceryListItem />, document.getElementById("app"));
+
